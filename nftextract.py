5d46157d423775ec9876da0d8d3705a7c0dc907b
adnetworkmaster <96611270+adnetworkmaster@users.noreply.github.com>
2022-01-09 11:46:11 +0530

    Update nftextract.py

--- nftextract.py
+++ nftextract.py
@@ -3,8 +3,8 @@ import pymongo
 import urllib
 
 client = pymongo.MongoClient("mongodb://ajar:" + urllib.parse.quote_plus("Raja@1802") + "@cluster0-shard-00-00.1vax0.mongodb.net:27017,cluster0-shard-00-01.1vax0.mongodb.net:27017,cluster0-shard-00-02.1vax0.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-umkr09-shard-0&authSource=admin&retryWrites=true&w=majority")
-mydb = client.nft
-mycol = mydb["tiffany_urls"]
+mydb = client.anime
+mycol = mydb["gogoapi"]
 mydoc = mycol.find()
 print(mydoc)
 # df = json_normalize(mydoc)
@@ -31,8 +31,10 @@ servers = ["https://p-001.herokuapp.com/crawl.json","https://p-002.herokuapp.com
 
 def requ(df):
   chossen = random.choice(servers)
-  print(df)
-  response = s.get( chossen + "?spider_name=tiffany_products&url=" + df)
+  df = df.replace(" ", "")
+  url = f"https://www3.gogoanime.cm{df}"
+  print(url)
+  response = s.get( chossen + "?spider_name=ggspider_dat&url=" + url)
   # print("request completed")
 
 import pandas as pd
@@ -41,9 +43,7 @@ pd.set_option('display.max_colwidth',1000)
 import multiprocessing
 import numpy as np
 pool = multiprocessing.Pool(15)
-df_2 = list(df["url"])
-# pool = multiprocessing.Pool(processes=5)
-# df_split = np.array_split(df, 5)
+df_2 = list(df["ep_url"])
 outputs = pool.map(requ, df_2)
 pool.close()
 pool.join()
