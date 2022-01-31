94953f6e5a511b14d1787410fdc8e58196c2c718
adnetworkmaster <96611270+adnetworkmaster@users.noreply.github.com>
2022-01-31 16:58:20 +0530

    Update nftextract.py

--- nftextract.py
+++ nftextract.py
@@ -4,7 +4,7 @@ import urllib
 
 client = pymongo.MongoClient("mongodb://ajar:" + urllib.parse.quote_plus("Raja@1802") + "@cluster0-shard-00-00.1vax0.mongodb.net:27017,cluster0-shard-00-01.1vax0.mongodb.net:27017,cluster0-shard-00-02.1vax0.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-umkr09-shard-0&authSource=admin&retryWrites=true&w=majority")
 mydb = client.nft
-mycol = mydb["versace_urls"]
+mycol = mydb["northface_urls"]
 mydoc = mycol.find()
 print(mydoc)
 # df = json_normalize(mydoc)
@@ -14,7 +14,7 @@ df = json_normalize(mydoc)
 
 print(df.shape)
 
-df = df.iloc[60000:75000]
+df = df.iloc[:15000]
 # df
 
 
@@ -25,14 +25,14 @@ import requests
 import random
 import time
 s = requests.Session()
-servers = ["https://p-001.herokuapp.com/crawl.json","https://p-002.herokuapp.com/crawl.json",
-           "https://p-003.herokuapp.com/crawl.json","https://p-004.herokuapp.com/crawl.json",
-           "https://p-005.herokuapp.com/crawl.json",]
+servers = ["https://u-001.herokuapp.com/crawl.json","https://u-002.herokuapp.com/crawl.json",
+           "https://u-003.herokuapp.com/crawl.json","https://u-004.herokuapp.com/crawl.json",
+           "https://u-005.herokuapp.com/crawl.json",]
 
 def requ(df):
   chossen = random.choice(servers)
   print(df)
-  response = s.get( chossen + "?spider_name=versace_products&url=" + df)
+  response = s.get( chossen + "?spider_name=northface_products&url=" + df)
   # print("request completed")
 
 import pandas as pd
